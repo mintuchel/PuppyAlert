be15f9367faa167acac51e317727746ee54e59f3
mintuchel <max3627@naver.com>
2024-07-15 16:59:14 +0900

    boilerplate

---
+++ src/main/java/seominkim/puppyAlert/PuppyAlertApplication.java
@@ -0,0 +1,13 @@
+package seominkim.puppyAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyAlertApplication.class, args);
+	}
+
+}
