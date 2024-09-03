ad640cc6a9a5af6b4f57198ccd7b308859a6ec08
mintuchel <max3627@naver.com>
2024-09-03 21:50:40 +0900

    refactor: entity relation

--- src/main/java/seominkim/puppyAlert/domain/market/entity/Market.java
+++ src/main/java/seominkim/puppyAlert/domain/market/entity/Market.java
@@ -14,7 +14,8 @@ import java.util.List;
 @Setter
 public class Market {
     @Id @GeneratedValue
-    private int marketId;
+    @Column(name="market_id")
+    private int id;
 
     @Column(columnDefinition = "varchar(225)", nullable=false)
     private String name;
@@ -31,6 +32,10 @@ public class Market {
     @Column(columnDefinition = "varchar(225)", nullable = false)
     private String imageURL;
 
-    @OneToMany(mappedBy="market", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    // 일대다 관계에서 일쪽을 주인으로 하고
+    // Shop에 Market의 PK값을 Market에서 FK로 market_id라는 칼럼명으로 놓기
+    // CascadeType.All로 영속성 정의 -> Shop 객체까지 persist를 자동으로 다 해줌
+    @OneToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="market_id")
     private List<Shop> shopList = new ArrayList<>();
 }
