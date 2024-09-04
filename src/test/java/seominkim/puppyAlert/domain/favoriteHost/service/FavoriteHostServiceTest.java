f14724ed9fd3699e2c685425ba1bd8e58b76100a
mintuchel <max3627@naver.com>
2024-09-04 23:18:56 +0900

    test: new ec2 repo

--- src/test/java/seominkim/puppyAlert/domain/favoriteHost/service/FavoriteHostServiceTest.java
+++ src/test/java/seominkim/puppyAlert/domain/favoriteHost/service/FavoriteHostServiceTest.java
@@ -22,6 +22,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+// 진짜 왜이러는거야
 @ExtendWith(MockitoExtension.class)
 public class FavoriteHostServiceTest {
     @InjectMocks
