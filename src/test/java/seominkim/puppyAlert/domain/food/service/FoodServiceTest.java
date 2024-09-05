5951331dade99d713acd581793e93b18a7107e9c
mintuchel <max3627@naver.com>
2024-09-06 03:54:32 +0900

    fix: servicelayer tests

--- src/test/java/seominkim/puppyAlert/domain/food/service/FoodServiceTest.java
+++ src/test/java/seominkim/puppyAlert/domain/food/service/FoodServiceTest.java
@@ -3,124 +3,193 @@ package seominkim.puppyAlert.domain.food.service;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import seominkim.puppyAlert.domain.favoriteHost.service.FavoriteHostService;
+import seominkim.puppyAlert.domain.food.dto.response.FoodInfoResponse;
 import seominkim.puppyAlert.domain.food.entity.Food;
+import seominkim.puppyAlert.domain.food.repository.FoodRepository;
 import seominkim.puppyAlert.domain.menu.entity.Menu;
 import seominkim.puppyAlert.domain.food.entity.FoodStatus;
+import seominkim.puppyAlert.domain.menu.service.MenuService;
 import seominkim.puppyAlert.domain.user.entity.User;
 import seominkim.puppyAlert.global.entity.Location;
 import seominkim.puppyAlert.global.entity.UserType;
+import seominkim.puppyAlert.global.utility.FoodLimitator;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
-@SpringBootTest
-@ActiveProfiles("test")
+import static org.mockito.BDDMockito.given;
+
+//@SpringBootTest
+//@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class FoodServiceTest {
+    @InjectMocks
+    FoodService foodService;
 
-    // hello
-    @Autowired FoodService foodService;
-    @Autowired EntityManager em;
+    @Mock FoodRepository foodRepository;
+    @Mock FoodLimitator foodLimitator;
+    @Mock FavoriteHostService favoriteHostService;
+    @Mock MenuService menuService;
+
+    private Food food;
+    private int foodId;
+    private FoodInfoResponse response;
 
     @BeforeEach
-    public void initTestDummy(){
-        User host1 = new User();
-        host1.setId("Ronaldo");
-        host1.setName("호날두");
-        host1.setNickName("iam노쇼에요");
-        host1.setPassword("7");
-        host1.setBirth(LocalDate.now());
-        host1.setAddress("레알 마드리드");
-        host1.setDetailAddress("산티아고 베르나베우");
-        host1.setLocation(new Location(100.5, 100.1));
-        host1.setPhoneNumber("010-4822-3636");
-        host1.setUserType(UserType.HOST);
+    private void testSetUp(){
+        food = new Food();
+        food.setFoodId(23L);
 
-        User host2 = new User();
-        host2.setId("Neymar");
-        host2.setName("네이마르");
-        host2.setNickName("밥은묵고다니냐");
-        host2.setPassword("11");
-        host2.setBirth(LocalDate.now());
-        host2.setAddress("파리생제르망");
-        host2.setDetailAddress("구장 이름 까먹음");
-        host2.setLocation(new Location(100.5, 100.1));
-        host2.setPhoneNumber("010-4465-8798");
-        host2.setUserType(UserType.HOST);
+        response = new FoodInfoResponse(
+                23L,
+                "son",
+                "suppersonny",
+                false,
+                "타코야키",
+                "www.abc.com",
+                LocalDateTime.now(),
+                "tottenham",
+                "hotspur",
+                new Location(37.7749, -122.4194),
+                FoodStatus.READY
+        );
+    }
 
-        User user1 = new User();
-        user1.setId("Messi");
-        user1.setName("메시");
-        user1.setNickName("요리조리비사이로막가드리블러");
-        user1.setPassword("10");
-        user1.setBirth(LocalDate.now());
-        user1.setAddress("바르셀로나");
-        user1.setDetailAddress("캄프누");
-        user1.setLocation(new Location(200.3, 200.2));
-        user1.setPhoneNumber("010-1111-2222");
-        user1.setUserType(UserType.PUPPY);
+//    @Test
+//    @DisplayName("집밥 조회")
+//    public void FindByIdTest(){
+//        // given
+//        given(foodRepository.findById(23L)).willReturn(Optional.of(food));
+//
+//        // when
+//        FoodInfoResponse foodInfoResponse = foodService.findById(23L);
+//
+//        // then
+//        Assertions.assertThat(foodInfoResponse.foodId()).isEqualTo(23L);
+//    }
 
-        Menu menu1 = new Menu();
-        menu1.setMenuName("testMenu1");
-        menu1.setImageURL("testURL1");
+    @Test
+    @DisplayName("집밥 추가")
+    public void addZipbobTest(){
 
-        Menu menu2 = new Menu();
-        menu2.setMenuName("testMenu2");
-        menu2.setImageURL("testURL2");
-
-        em.persist(host1);
-        em.persist(host2);
-        em.persist(user1);
-        em.persist(menu1);
-        em.persist(menu2);
     }
 
     @Test
-    @Transactional
-    @Rollback
-    public void getHistoryTest(){
-        // given
-        User host1 = em.find(User.class, "Ronaldo");
-        User host2 = em.find(User.class, "Neymar");
+    @DisplayName("가능한 집밥 조회")
+    public void getAvailableZipbobTest(){
 
-        System.out.println(host1.getHostFoods());
-
-        // when
-        Assertions.assertThat(host2.getHostFoods().size()).isEqualTo(0);
     }
 
-    @Test
-    @Transactional
-    @Rollback
-    public void getMostRecentFoodTest(){
-
-        // given
-        Food food1 = new Food();
-        food1.setHost(em.find(User.class, "Ronaldo"));
-        food1.setPuppy(em.find(User.class, "Messi"));
-        food1.setMenu(em.find(Menu.class, "testMenu1"));
-        food1.setTime(LocalDateTime.of(2020,6,18,8,24,16));
-        food1.setStatus(FoodStatus.MATCHED);
-
-        Food food2 = new Food();
-        food2.setHost(em.find(User.class, "Ronaldo"));
-        food2.setPuppy(em.find(User.class, "Messi"));
-        food2.setMenu(em.find(Menu.class, "testMenu2"));
-        food2.setTime(LocalDateTime.of(2024,7,20,5,50,32));
-        food2.setStatus(FoodStatus.MATCHED);
-
-        em.persist(food1);
-        em.persist(food2);
-
-        // when
-        Food mostRecentFood = foodService.getMostRecentFood("Messi","Ronaldo");
-
-        // then
-        Assertions.assertThat(mostRecentFood).isEqualTo(food2);
-    }
+//    @Autowired FoodService foodService;
+//    @Autowired EntityManager em;
+//
+//    @BeforeEach
+//    public void initTestDummy(){
+//        User host1 = new User();
+//        host1.setId("Ronaldo");
+//        host1.setName("호날두");
+//        host1.setNickName("iam노쇼에요");
+//        host1.setPassword("7");
+//        host1.setBirth(LocalDate.now());
+//        host1.setAddress("레알 마드리드");
+//        host1.setDetailAddress("산티아고 베르나베우");
+//        host1.setLocation(new Location(100.5, 100.1));
+//        host1.setPhoneNumber("010-4822-3636");
+//        host1.setUserType(UserType.HOST);
+//
+//        User host2 = new User();
+//        host2.setId("Neymar");
+//        host2.setName("네이마르");
+//        host2.setNickName("밥은묵고다니냐");
+//        host2.setPassword("11");
+//        host2.setBirth(LocalDate.now());
+//        host2.setAddress("파리생제르망");
+//        host2.setDetailAddress("구장 이름 까먹음");
+//        host2.setLocation(new Location(100.5, 100.1));
+//        host2.setPhoneNumber("010-4465-8798");
+//        host2.setUserType(UserType.HOST);
+//
+//        User user1 = new User();
+//        user1.setId("Messi");
+//        user1.setName("메시");
+//        user1.setNickName("요리조리비사이로막가드리블러");
+//        user1.setPassword("10");
+//        user1.setBirth(LocalDate.now());
+//        user1.setAddress("바르셀로나");
+//        user1.setDetailAddress("캄프누");
+//        user1.setLocation(new Location(200.3, 200.2));
+//        user1.setPhoneNumber("010-1111-2222");
+//        user1.setUserType(UserType.PUPPY);
+//
+//        Menu menu1 = new Menu();
+//        menu1.setMenuName("testMenu1");
+//        menu1.setImageURL("testURL1");
+//
+//        Menu menu2 = new Menu();
+//        menu2.setMenuName("testMenu2");
+//        menu2.setImageURL("testURL2");
+//
+//        em.persist(host1);
+//        em.persist(host2);
+//        em.persist(user1);
+//        em.persist(menu1);
+//        em.persist(menu2);
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback
+//    public void getHistoryTest(){
+//        // given
+//        User host1 = em.find(User.class, "Ronaldo");
+//        User host2 = em.find(User.class, "Neymar");
+//
+//        System.out.println(host1.getHostFoods());
+//
+//        // when
+//        Assertions.assertThat(host2.getHostFoods().size()).isEqualTo(0);
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback
+//    public void getMostRecentFoodTest(){
+//
+//        // given
+//        Food food1 = new Food();
+//        food1.setHost(em.find(User.class, "Ronaldo"));
+//        food1.setPuppy(em.find(User.class, "Messi"));
+//        food1.setMenu(em.find(Menu.class, "testMenu1"));
+//        food1.setTime(LocalDateTime.of(2020,6,18,8,24,16));
+//        food1.setStatus(FoodStatus.MATCHED);
+//
+//        Food food2 = new Food();
+//        food2.setHost(em.find(User.class, "Ronaldo"));
+//        food2.setPuppy(em.find(User.class, "Messi"));
+//        food2.setMenu(em.find(Menu.class, "testMenu2"));
+//        food2.setTime(LocalDateTime.of(2024,7,20,5,50,32));
+//        food2.setStatus(FoodStatus.MATCHED);
+//
+//        em.persist(food1);
+//        em.persist(food2);
+//
+//        // when
+//        Food mostRecentFood = foodService.getMostRecentFood("Messi","Ronaldo");
+//
+//        // then
+//        Assertions.assertThat(mostRecentFood).isEqualTo(food2);
+//    }
 }
--- src/test/java/seominkim/puppyAlert/domain/menu/service/MenuServiceTest.java
+++ src/test/java/seominkim/puppyAlert/domain/menu/service/MenuServiceTest.java
@@ -1,42 +1,86 @@
 package seominkim.puppyAlert.domain.menu.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import seominkim.puppyAlert.domain.menu.entity.Menu;
+import seominkim.puppyAlert.domain.menu.repository.MenuRepository;
+import seominkim.puppyAlert.global.utility.ImageCrawler;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 public class MenuServiceTest {
+    @InjectMocks
+    private MenuService menuService;
 
-    @Autowired MenuService menuService;
+    @Mock
+    private MenuRepository menuRepository;
+
+    @Mock
+    private ImageCrawler imageCrawler;
+
+    private Menu menu;
+    private String menuName = "콩국수";
+
+    @BeforeEach
+    private void testSetUp(){
+        menu = new Menu();
+        menu.setMenuName(menuName);
+    }
 
     @Test
-    @Transactional
-    @Rollback
-    public void menuServiceTest(){
+    @DisplayName("메뉴 조회")
+    void findMenuTest(){
+        // given
+        given(menuRepository.existsById(menuName)).willReturn(true);
+        given(menuRepository.findById(menuName)).willReturn(Optional.of(menu));
 
-        String keyword = "피자";
-        System.out.println(menuService.findOne(keyword).getImageURL());
+        // when
+        Menu savedMenu = menuService.findOne(menuName);
 
-        keyword = "햄버거";
-        System.out.println(menuService.findOne(keyword).getImageURL());
-
-        keyword = "삼계탕";
-        System.out.println(menuService.findOne(keyword).getImageURL());
-
-        keyword = "제육볶음";
-        System.out.println(menuService.findOne(keyword).getImageURL());
-
-        keyword = "콩국수";
-        System.out.println(menuService.findOne(keyword).getImageURL());
-
-        keyword = "비빔밥";
-        System.out.println(menuService.findOne(keyword).getImageURL());
-
-        // 중복된 키워드 들어왔을때 URL DB에서 끌어오는지 확인
-        String protoURL = menuService.findOne("콩국수").getImageURL();
-        Assertions.assertThat(protoURL).isEqualTo(menuService.findOne("콩국수").getImageURL());
+        Assertions.assertThat(savedMenu.getMenuName()).isEqualTo(menu.getMenuName());
     }
+
+//
+//    @Autowired MenuService menuService;
+//
+//    @Test
+//    @Transactional
+//    @Rollback
+//    public void menuServiceTest(){
+//
+//        String keyword = "피자";
+//        System.out.println(menuService.findOne(keyword).getImageURL());
+//
+//        keyword = "햄버거";
+//        System.out.println(menuService.findOne(keyword).getImageURL());
+//
+//        keyword = "삼계탕";
+//        System.out.println(menuService.findOne(keyword).getImageURL());
+//
+//        keyword = "제육볶음";
+//        System.out.println(menuService.findOne(keyword).getImageURL());
+//
+//        keyword = "콩국수";
+//        System.out.println(menuService.findOne(keyword).getImageURL());
+//
+//        keyword = "비빔밥";
+//        System.out.println(menuService.findOne(keyword).getImageURL());
+//
+//        // 중복된 키워드 들어왔을때 URL DB에서 끌어오는지 확인
+//        String protoURL = menuService.findOne("콩국수").getImageURL();
+//        Assertions.assertThat(protoURL).isEqualTo(menuService.findOne("콩국수").getImageURL());
+//    }
 }
