5dbc2a5b6e6ab0eea47172e9d4540591b06db696
mintuchel <max3627@naver.com>
2024-09-06 17:52:09 +0900

    update: tests

--- src/test/java/seominkim/puppyAlert/PuppyAlertApplicationTests.java
+++ src/test/java/seominkim/puppyAlert/PuppyAlertApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class PuppyAlertApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 }
