01b1a9362c7b78bf020ff5e6fc7c48f034050b37
mintuchel <max3627@naver.com>
2024-09-14 22:03:28 +0900

    update: comment

--- src/main/java/seominkim/puppyAlert/domain/food/repository/FoodRepository.java
+++ src/main/java/seominkim/puppyAlert/domain/food/repository/FoodRepository.java
@@ -11,10 +11,11 @@ import java.util.Optional;
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
-    // JPQL 에는 LIMIT 절이 없으므로 Pageable 로 해결해야함
-    // @Query("SELECT z FROM Food z WHERE z.puppy.id = :puppyId AND z.host.id = :hostId ORDER BY z.time DESC")
-    // List<Food> findMostRecentByPuppyIdAndHostId(@Param("puppyId") String puppyId, @Param("hostId") String hostId, Pageable pageable);
-
     @Query(value = "SELECT * FROM food WHERE puppy_id = :pid AND host_id = :hid ORDER BY time DESC LIMIT 1;", nativeQuery = true)
     Optional<Food> findMostRecentByPuppyIdAndHostId(@Param("pid") String puppyId, @Param("hid") String hostId);
 }
+
+
+// JPQL 에는 LIMIT 절이 없으므로 Pageable 로 해결해야함
+// @Query("SELECT z FROM Food z WHERE z.puppy.id = :puppyId AND z.host.id = :hostId ORDER BY z.time DESC")
+// List<Food> findMostRecentByPuppyIdAndHostId(@Param("puppyId") String puppyId, @Param("hostId") String hostId, Pageable pageable);
